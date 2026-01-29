203fc706f6237eb838724878637ad6392536a5c1
Oxunjon <oxunjonsharipov0422@gmail.com>
2026-01-29 13:40:41 +0500

    bug fix

--- handlers/user.py
+++ handlers/user.py
@@ -482,6 +482,7 @@ async def show_results(message: Message, db: Database):
 
 # ==================== MA'LUMOT ====================
 
+
 @router.message(F.text == "ℹ️ Ma'lumot")
 async def show_info(message: Message, db: Database):
     """Bot haqida ma'lumot"""
